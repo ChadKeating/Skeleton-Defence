2863aef2742084c2f8b194af8a7f60feb1cfb1e9
Chad Keating <deadlysin4@gmail.com>
2015-02-06 03:42:35 +0000

    Cleaned up some assets

---
+++ Assets/Prefabs/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	Animator anim;
+	public float moveSpeed = 0.5f;
+	public GameObject destroyEffect;
+
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		health = (int)(Random.Range(6, 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("Speed", moveSpeed);
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Projectile") {
+			health -= 2;
+		}
+		if (health <= 0) {
+			Instantiate(destroyEffect, transform.position, transform.rotation);
+			Destroy(this.gameObject);
+		}
+	}
+}
